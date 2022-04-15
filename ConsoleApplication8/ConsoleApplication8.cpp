49928dc4d119d3e92052510cf35bca5e8df3eda8
User <User@WIN-A4TOTEJDR9Q>
2022-04-15 21:47:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+   int j=0;
+    setlocale(LC_ALL, "Russian");
+    cout << "Эта программа демонстрирует печатные возможности языка C++.\n\n";
+    string days[7] = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+    do
+        for (int i = 0; i < 7; i++) {
+            j++;
+                cout << days[i]<<"\t\t"<<j << endl;
+            }
+    while (j<364); 
+    return 0;
+}
+
+
